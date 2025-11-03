706c1e6428e07b9b978c1fa52c6f069e79d87d13
rvstarts <vikash.23b1531038@abes.ac.in>
2025-11-03 10:24:51 +0530

    image maipulation

--- ReactViteApps/reactprops/src/components/ImageManipulation.jsx
+++ ReactViteApps/reactprops/src/components/ImageManipulation.jsx
@@ -4,12 +4,19 @@ function ImageManipulation() {
   return (
     <div>
       <h2>Image Manipulation</h2>
-      <img 
-        src="https://www.bing.com/th/id/OIP.wOIYcd3giZ-DTkfzFugylwHaH6?w=167&h=211&c=8&rs=1&qlt=90&o=6&dpr=1.3&pid=3.1&rm=2" 
-        height={200} 
-        width={300} 
-        alt="cat image"
-      />
+      <div style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        margin: '20px auto'
+      }}>
+        <img 
+          src="https://www.bing.com/th/id/OIP.wOIYcd3giZ-DTkfzFugylwHaH6?w=167&h=211&c=8&rs=1&qlt=90&o=6&dpr=1.3&pid=3.1&rm=2" 
+          height={200} 
+          width={300} 
+          alt="cat image"
+        />
+      </div>
     </div>
   )
 }
