8fe67147cfad5b5f11b6fd5f6214d3960f79e77c
rvstarts <vikash.23b1531038@abes.ac.in>
2025-11-03 10:21:38 +0530

    statehandlle

---
+++ ReactViteApps/reactprops/src/components/ImageManipulation.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function ImageManipulation() {
+  return (
+    <div>
+      <h2>Image Manipulation</h2>
+      <img 
+        src="https://www.bing.com/th/id/OIP.wOIYcd3giZ-DTkfzFugylwHaH6?w=167&h=211&c=8&rs=1&qlt=90&o=6&dpr=1.3&pid=3.1&rm=2" 
+        height={200} 
+        width={300} 
+        alt="cat image"
+      />
+    </div>
+  )
+}
+
+export default ImageManipulation
