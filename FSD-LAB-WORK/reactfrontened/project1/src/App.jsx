439e08141304c1ad118d595db0a60ddffeb64b0d
rvstarts <vikash.23b1531038@abes.ac.in>
2026-02-23 10:05:22 +0530

    file system

--- FSD-LAB-WORK/reactfrontened/project1/src/App.jsx
+++ FSD-LAB-WORK/reactfrontened/project1/src/App.jsx
@@ -4,21 +4,44 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-
   const[data,setData]=useState([]);
+  const[loader,setLoader]=useState(false)
+async function getData(){
 
-  async function getData(){
-    const response = await fetch('http://localhost:4007/data');
-    const jsondata = await response.json();
-    setData(jsondata);
-    console.log(jsondata.msg);
+  try{
+    setLoader(true)
+       const response=await fetch('http://localhost:4007/data');
+       const jsondata=await response.json();
+       setData(jsondata.msg);
+  }catch(e){
+    console.log(e)
+  }
+  finally{
+     setLoader(false)
   }
 
+  
+}
+
+function cartData(dataitem){
+alert(dataitem.title)
+}
   return (
     <>
-     <h2>Welcome to React app</h2>
-     {JSON.stringify(data)}
-     <button onClick={getData}>fetchdata</button>
+      <h2>Welocme to React App</h2>
+      {
+          data.map((ele)=>(
+          <div style={{border:'2px solid red'}}>
+            <img src={ele.image} height={200} width={200}></img>
+            <h2>{ele.id}:{ele.title}</h2>
+            <h3>{ele.title}</h3>
+            <button onClick={()=>cartData(ele)}>Add to cart</button>
+          </div>
+        ))
+      }
+      {loader?(<h2 style={{color:'red'}}>Data is loading...</h2>):("")}
+      {/* {JSON.stringify(data)} */}
+      <button onClick={getData}>FetchData</button>
     </>
   )
 }
